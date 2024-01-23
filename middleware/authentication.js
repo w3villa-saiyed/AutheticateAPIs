0c7296d22cbccb9601809abe037411a6dbc2b139
w3villa-saiyed <saiyed.adnanali@w3villa.com>
2024-01-23 15:41:08 +0530

    push_check

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -6,6 +6,7 @@ const asyncHandler = require('express-async-handler');
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
+  
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
       token = req.headers.authorization.split(' ')[1];
