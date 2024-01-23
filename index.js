b5a578a13e3b55aaee683906e09af343412d4812
w3villa-saiyed <saiyed.adnanali@w3villa.com>
2024-01-23 16:17:34 +0530

    some_url_chnages

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const userRoutes = require('./routers/users_routes')
 const app = express();
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const mongoose = require('./config/database_connection');
 require('dotenv').config();
 
 mongoose();
 app.use(express.json());
-
+app.use(
+    '/register',
+    createProxyMiddleware({
+      target: 'https://testingapisr.onrender.com',
+      changeOrigin: true,
+    })
+  );
+  
 app.use('/',userRoutes)
 
 app.listen(1000,()=>{
